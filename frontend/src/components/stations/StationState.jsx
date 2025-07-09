b177e43edd704b5692de58b2b6f438043773efd2
alessandromarcellini <alessandromarcellini49@gmail.com>
2025-07-09 18:06:28 +0200

    Refactored StationState into its own component

---
+++ frontend/src/components/stations/StationState.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import Switch from 'rc-switch';
+import 'rc-switch/assets/index.css';
+import axios from 'axios';
+
+import { useParams } from 'react-router-dom';
+
+
+function StationState() {
+    const [stationState, setStationState] = useState(true);
+
+    const { stationId } = useParams();
+    const stationIdInt = parseInt(stationId);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/station/${stationIdInt}/currentState`)
+            .then(
+                response => {
+                    setStationState(response.data);
+                }
+            )
+            .catch(err => console.error('Error fetching station:', err));
+    });
+
+    const handleStationStateChange = (value) => {
+        // TODO call the api and wait for it's response, if no errors => setStationState
+        axios.post(
+            `http://localhost:8000/api/station/${stationIdInt}/setState`,
+            {
+                "new_state": value ? 'active' : 'inactive'
+            }
+        ).then(
+            response => {
+                if (response.status === 200) {
+                    console.log("Toogled station state to " + value ? "active" : "inactive")
+                    setStationState(value);
+                } else {
+                    console.error('Unexpected response:', response);
+                }
+            }
+        ).catch(err => console.error('Error changing station state:', err));
+    };
+
+
+    return (
+        <>
+            <h3>Station state: </h3>
+            <Switch
+                checked={stationState}
+                onChange={handleStationStateChange}
+            />
+        </>
+    );
+}
+
+export default StationState;
--- frontend/src/pages/admin/StationDetails.jsx
+++ frontend/src/pages/admin/StationDetails.jsx
@@ -4,8 +4,9 @@ import Switch from 'rc-switch';
 import 'rc-switch/assets/index.css';
 import axios from 'axios';
 
+import StationState from '../../components/stations/StationState'
+
 function StationDetails() {
-    const [stationState, setStationState] = useState(true);
     const [checkedStates, setCheckedStates] = useState({});
     const [station, setStation] = useState(null);
     const { stationId } = useParams();
@@ -15,25 +16,6 @@ function StationDetails() {
         //TODO call the api to set the sensor's state
         setCheckedStates(prev => ({ ...prev, [sensorId]: value }));
         console.log(`Sensor ${sensorId} is now:`, value);
-    };
-
-    const handleStationStateChange = (value) => {
-        // TODO call the api and wait for it's response, if no errors => setStationState
-        axios.post(
-            `http://localhost:8000/api/station/${stationIdInt}/setState`,
-            {
-                "new_state": value ? 'active' : 'inactive'
-            }
-        ).then(
-            response => {
-                if (response.status === 200) {
-                    console.log("Toogled station state to " + value ? "active" : "inactive")
-                    setStationState(value);
-                } else {
-                    console.error('Unexpected response:', response);
-                }
-            }
-        ).catch(err => console.error('Error changing station state:', err));
     };
 
     if (isNaN(stationIdInt)) {
@@ -50,7 +32,6 @@ function StationDetails() {
             .then(
                 response => {
                     setStation(response.data);
-                    setStationState(response.data.state == 'active');
                 }
             )
             .catch(err => console.error('Error fetching station:', err));
@@ -61,11 +42,7 @@ function StationDetails() {
     return (
         <>
             <h1>Station Details: {station.name}</h1>
-            <h3>Station state: </h3>
-            <Switch
-                checked={stationState}
-                onChange={handleStationStateChange}
-            />
+            <StationState />
 
             <ul>
                 {station.sensors?.map(sensor => (
